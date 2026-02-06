2f33109ae3541df993f33d0fdf72ea11e05fff78
tgapk <914837973@qq.com>
2026-02-06 16:31:00 +0800

    update:修改mongo连接到远程服务器

--- src/infrastructure/database.ts
+++ src/infrastructure/database.ts
@@ -3,7 +3,15 @@ import mongoose from 'mongoose';
 export const connectDB = async (): Promise<void> => {
   try {
     const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/device-entry';
-    await mongoose.connect(mongoURI);
+    const user = process.env.MONGO_USER;
+    const pass = process.env.MONGO_PWD;
+    const options: mongoose.ConnectOptions = {
+      authSource: 'admin'
+    };
+    if (user) options.user = user;
+    if (pass) options.pass = pass;
+    
+    await mongoose.connect(mongoURI, options);
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('MongoDB connection error:', err);
