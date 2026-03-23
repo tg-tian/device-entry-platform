c2bf333e5f20290556e4bc6e407aaa12ef70208f
tgapk <914837973@qq.com>
2026-03-23 22:12:14 +0800

    add:增加pm2启动文件

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [
+        {
+            name: 'mte-device-entry',
+            cwd: '/root/mte-device-entry-platform',
+            script: 'node_modules/.bin/ts-node',
+            args: 'src/main.ts',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '512M',
+            env: {
+                NODE_ENV: 'production'
+            },
+            error_file: '/root/solo-mte/logs/mte-device-entry-error.log',
+            out_file: '/root/solo-mte/logs/mte-device-entry-out.log',
+            time: true
+        }
+    ]
+};
